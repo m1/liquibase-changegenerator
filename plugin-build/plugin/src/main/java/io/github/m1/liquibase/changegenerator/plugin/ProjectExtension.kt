ecc10a51a312c7ff7a4e4cf5b25c8476385b3985
Miles Croxford <hello@milescroxford.com>
2022-03-30 21:47:07 +0100

    comments

--- plugin-build/plugin/src/main/java/io/github/m1/liquibase/changegenerator/plugin/ProjectExtension.kt
+++ plugin-build/plugin/src/main/java/io/github/m1/liquibase/changegenerator/plugin/ProjectExtension.kt
@@ -2,5 +2,11 @@ package io.github.m1.liquibase.changegenerator.plugin
 
 import org.gradle.api.Project
 
+/**
+ * Inject the ChangeGeneratorPluginExtension into the gradle Project
+ *
+ * @see ChangeGeneratorPluginExtension
+ * @see Project
+ */
 val Project.changeGeneratorPluginExtension: ChangeGeneratorPluginExtension
     get() = extensions.getByType(ChangeGeneratorPluginExtension::class.java)
